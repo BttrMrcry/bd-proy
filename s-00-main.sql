d40b21677985c4cf6d57933693b2ff6ee4a16df8
BttrMrcry <bettermercury@comunidad.unam.mx>
2022-01-02 21:07:24 -0600

    Cambios en las llamadas a scripts en el script princial

--- s-00-main.sql
+++ s-00-main.sql
@@ -1,6 +1,20 @@
---@Autor(es): López Becerra Ricardo, Martinez Reyes Javier
+--@Autor(es): López Becerra Ricardo
 --@Fecha creación: 27/12/2021
 --@Descripción: Script de ejecución principal
 
+prompt ejecutando script s-01-usuarios 
 @s-01-usuarios.sql 
+
+prompt ejecutando script s-02-entidades
 @s-02-entidades.sql
+
+prompt ejecutando script s-04-tablas-externas
+@s-04-tablas-externas.sql
+
+prompt ejecutando script s-03-tablas-temporales
+@s-03-tablas-temporales.sql
+
+prompt ejecutando script s-05-secuencias
+@s-05-secuencias.sql
+
+
