f031b97297a9ebed468907d537d1deb3d31c2aa8
BttrMrcry <bettermercury@comunidad.unam.mx>
2022-01-02 21:06:56 -0600

    Creación de las secuencias necesarias para llenar las tablas

---
+++ s-05-secuencias.sql
@@ -0,0 +1,98 @@
+--@Autor(es): López Becerra Ricardo
+--@Fecha creación: 02/01/2022
+--@Descripción: Código para generar las secuencias necesarias 
+
+prompt Conectando con el usuario administrador
+connect l_proy_admin/contrasenia
+show user
+
+create sequence seq_aeronave
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_pasajero
+    start with 1
+    increment by 1
+    nocycle
+    cache 5;
+
+create sequence seq_vuelo_pasajero
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_vuelo
+    start with 1
+    increment by 1
+    nocycle
+    cache 5;
+
+create sequence seq_aeropuerto
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_equipaje
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_pase_abordaje
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_rol_empleado
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_tripulacion
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_paquete
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_estado
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_historial_estado
+    start with 1
+    increment by 1
+    nocycle
+    cache 10;
+
+create sequence seq_puesto
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_empleado
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
+create sequence seq_tipo_paquete
+    start with 1
+    increment by 1
+    nocycle
+    nocache;
+
