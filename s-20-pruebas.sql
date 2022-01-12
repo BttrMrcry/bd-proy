d4c6d1a93debec64741e3cd96109e56636b0c1c3
BttrMrcry <bettermercury@comunidad.unam.mx>
2022-01-11 22:37:11 -0600

    Modificaciones finales al modelo

---
+++ s-20-pruebas.sql
@@ -0,0 +1,25 @@
+--@Autor(es): López Becerra Ricardo
+--@Fecha creación: 11/01/2022
+--@Descripción: script que lanza la prueba de los triggers, procedimientos y funciones
+
+
+prompt ejecutando script s-12-tr-exporta-elimimacion-prueba
+@s-12-tr-exporta-eliminacion-prueba.sql 
+
+prompt ejecutando s-12-tr-verifica-aeronave-prueba
+@s-12-tr-verifica-aeronave-prueba.sql
+
+prompt ejecutando s-14-p-registra-avion-prueba
+@s-14-p-registra-avion-prueba.sql
+
+prompt ejecutando s-14-p-registra-maleta-prueba
+@s-14-p-registra-maleta-prueba.sql 
+
+prompt ejecutando s-16-fx-calcula-distancia-prueba
+@s-16-fx-calcula-distancia-prueba.sql 
+
+prompt ejecutando s-16-fx-carga-bin-prueba
+@s-16-fx-carga-bin-prueba.sql 
+
+prompt ejecutando s-18-lob-carga-fotos-empleados-prueba
+@s-18-lob-carga-fotos-empleados-prueba.sql
