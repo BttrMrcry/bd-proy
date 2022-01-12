0d120b014f8bc4874485e927f3592aa0e1685481
BttrMrcry <bettermercury@comunidad.unam.mx>
2022-01-11 22:45:05 -0600

    LLama a los nuevos scripts y recoloca las pausas

--- s-00-main.sql
+++ s-00-main.sql
@@ -3,73 +3,142 @@
 --@Descripción: Script de ejecución principal
 
 prompt ejecutando script s-01-usuarios 
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-01-usuarios.sql 
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-02-entidades
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-02-entidades.sql
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-04-tablas-externas
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-04-tablas-externas.sql
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-05-secuencias
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-05-secuencias.sql
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-06-indices
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-06-indices.sql
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-07-sinonimos
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-07-sinonimos.sql
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-08-vistas
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-08-vistas.sql  
 
-begin
-dbms_session.sleep(3);
-end;
-/
+
 
 prompt ejecutando script s-09-carga-inicial 
+begin
+dbms_session.sleep(2);
+end;
+/
 @s-09-carga-inicial.sql
 
-begin
-dbms_session.sleep(1);
-end;
-/
 
 prompt ejecutando script s-03-tablas-temporales
-@s-03-tablas-temporales.sql
-
 begin
-dbms_session.sleep(1);
+dbms_session.sleep(2);
 end;
 /
+@s-03-tablas-temporales.sql
+
+prompt ejecutando script s-10-consultas
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-10-consultas.sql
+
+prompt ejecutando script s-11-tr-exporta-elimimacion
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-11-tr-exporta-elimimacion.sql 
+
+prompt ejecutando s-11-tr-verifica-aeronave
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-11-tr-verifica-aeronave.sql
+
+prompt ejecutando s-13-p-registra-avion
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-13-p-registra-avion.sql
+
+prompt ejecutando s-13-p-registra-maleta
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-13-p-registra-maleta.sql 
+
+prompt ejecutando s-15-fx-calcula-distancia
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-15-fx-calcula-distancia.sql 
+
+prompt ejecutando s-15-fx-carga-bin
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-15-fx-carga-bin.sql 
+
+prompt ejecutando s-17-lob-carga-fotos-empleados
+begin
+dbms_session.sleep(2);
+end;
+/
+@s-17-lob-carga-fotos-empleados.sql
+
+
+
+
+-- prompt ejecutando script s-17-lob-carga-fotos-empleados
+-- @s-17-lob-carga-fotos-empleados.sql 
+
