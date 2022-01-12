24d4669a616a8478447aedda7419840c2ab04658
BttrMrcry <bettermercury@comunidad.unam.mx>
2022-01-11 22:41:25 -0600

    Procedimiento que calcula la distancia recorrida en un vuelo

---
+++ s-15-fx-calcula-distancia.sql
@@ -0,0 +1,88 @@
+--@Autor(es): López Becerra Ricardo
+--@Fecha creación: 10/01/2022
+--@Descripción: Función que calcula la distancia recorrida en un vuelo
+connect l_proy_admin/contrasenia 
+
+set serveroutput on
+create or replace function calcula_distancia_vuelo
+(
+    p_vuelo_id in number
+)return number is 
+
+PI number := 3.1415926535;
+v_lat1 number;
+v_long1 number;
+v_lat2 number;
+v_long2 number;
+
+v_numero1 number;
+v_numero2 number;
+
+v_lat1_rad number;
+v_long1_rad number;
+v_lat2_rad number;
+v_long2_rad number;
+
+dif_lat number;
+dif_long number;
+
+v_distancia number := 0;
+
+R number := 6371; --Radio promedio de la tierra en kilometros
+a number;
+c number;
+
+cursor cur_points is
+    select numero,latitud,longitud 
+    from vuelo_ubicacion
+    where vuelo_id = p_vuelo_id
+    order by numero; 
+
+begin
+    open cur_points;
+
+    fetch cur_points into v_numero1, v_lat1, v_long1;
+    if cur_points%notfound then 
+        close cur_points;
+        return -1;
+    end if;
+    
+    loop
+        fetch cur_points into v_numero2, v_lat2, v_long2;
+    exit when cur_points%notfound;
+    dbms_output.put_line(
+        'Calculando disyancia entre los puntos' 
+        ||' numero: '||v_numero1
+        ||' latitud: '||v_lat1
+        ||' longitud: '||v_long1
+        ||' y'
+        ||' numero: '||v_numero2
+        ||' latitud: '||v_lat2
+        ||' longitud: '||v_long2 
+    );
+    
+    v_lat1_rad := v_lat1 * PI/180;
+    V_lat2_rad := v_lat2 * PI/180;
+    
+    --formula: http://www.movable-type.co.uk/scripts/latlong.html
+
+    dif_lat := (v_lat2-v_lat1) * PI/180;
+    dif_long := (v_long2-v_long1) * PI/180; 
+    a := power(sin(dif_lat/2),2) + cos(v_lat1_rad)*cos(v_lat2_rad)*
+        power(sin(dif_long/2),2);
+    
+    c := 2*atan2(sqrt(a),sqrt(1-a));
+
+    v_distancia := v_distancia + R*c; --distancia en kilometros.
+
+    v_numero1 := v_numero2;
+    v_lat1 := v_lat2;
+    v_long1 := v_long2;
+    
+    end loop;
+    
+    close cur_points; 
+    return v_distancia;
+end;
+/
+show errors
---
+++ s-16-fx-calcula-distancia-prueba.sql
@@ -0,0 +1,57 @@
+--@Autor(es): López Becerra Ricardo
+--@Fecha creación: 10/01/2022
+--@Descripción: Bloque anónimo que verifica el  
+--funcionamiento de la función calcula_distancia_vuelo
+--La prueba simula un avion dando la vuelta al mundo
+connect l_proy_admin/contrasenia 
+
+set serveroutput on
+declare 
+    v_llave number := seq_vuelo.nextval;
+    v_count number := 0;
+    v_distancia_calculada number;
+    v_distancia_esperada number := 40004;
+begin
+
+    insert into vuelo
+    values(
+        v_llave,sysdate,1,0,sysdate,sysdate + 1/24,99999,null,1,2,1,3
+    );
+    
+    insert into vuelo_ubicacion
+    values(1,v_llave,sysdate+1/96,0,0);
+    insert into vuelo_ubicacion
+    values(2,v_llave,sysdate+1/48,0,180);
+    insert into vuelo_ubicacion
+    values(3,v_llave,sysdate+1/24,0,0);
+
+    v_distancia_calculada := calcula_distancia_vuelo(v_llave);
+    dbms_output.put_line(v_distancia_calculada);
+    
+    if abs(v_distancia_calculada - v_distancia_esperada) > 200 then --Presicion del 0.5%
+        
+        dbms_output.put_line('Eliminando registros de pruebas anteriores');
+        delete from vuelo_ubicacion where vuelo_id = (
+            select vuelo_id from vuelo 
+            where numero_vuelo = 99999
+        );
+        delete from vuelo where numero_vuelo = 99999;
+        delete from vuelo_ubicacion where vuelo_id = v_llave;
+        delete from vuelo where vuelo_id = v_llave;
+
+        raise_application_error(-20003,'La distancia no es lo suficiente precisa');
+    end if;    
+    
+
+    dbms_output.put_line('Eliminando registros de pruebas anteriores');
+    delete from vuelo_ubicacion where vuelo_id = (
+        select vuelo_id from vuelo 
+        where numero_vuelo = 99999
+    );
+    delete from vuelo where numero_vuelo = 99999;
+    delete from vuelo_ubicacion where vuelo_id = v_llave;
+    delete from vuelo where vuelo_id = v_llave;
+
+    dbms_output.put_line('Prueba exitosa!');
+end;
+/
